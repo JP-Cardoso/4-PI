c8df88b3933507059d39ab851e82d842ee4eb09d
JP-Cardoso <jhonpoal@hotmail.com>
2023-06-15 16:31:01 -0300

    feat; add db

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 module.exports = {
     logging: console.log,
     dialect: process.env.DB_DIALECT, 
@@ -10,5 +9,12 @@ module.exports = {
     define: {
         timestamps: true,
         underscored: true,
-    }
+    },
+    dialectOptions: {
+        ssl: process.env.DB_SSL ? {
+          require: true,
+          rejectUnauthorized: false, // use this if you don't have a CA or self-signed certificate
+          ca:  process.env.DB_SSL,
+        } : null,
+      },
 };
